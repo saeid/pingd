e8f345493fb187664375188ddb79f9c2aae5b416
Saeid Basirnia <git@basirnia.me>
2026-04-11 09:25:58 +0200

    Update routes with public and protected routes

--- Sources/pingd/Startup/routes.swift
+++ Sources/pingd/Startup/routes.swift
@@ -1,10 +1,28 @@
 import Vapor
 
 func routes(_ app: Application, _ services: AppDependencies) throws {
-    try app.routes.register(
-        collection: HealthController(
-            healthClient: services.healthClient,
-            appInfo: services.appInfo
-        )
+    // Public
+    try app.routes.register(collection: HealthController(
+        healthClient: services.healthClient,
+        appInfo: services.appInfo
+    ))
+    try app.routes.register(collection: AuthController(
+        authFeature: services.authFeature,
+        tokenClient: services.tokenClient
+    ))
+
+    // Protected
+    let protected = app.routes.grouped(
+        TokenAuthMiddleware(tokenClient: services.tokenClient, now: services.now)
     )
+    protected.get("me") { req in
+        try UserResponse(req.user)
+    }
+    try protected.register(collection: UserController(
+        userFeature: services.userFeature,
+        authClient: services.authClient
+    ))
+    try protected.register(collection: TokenController(
+        tokenFeature: services.tokenFeature
+    ))
 }
