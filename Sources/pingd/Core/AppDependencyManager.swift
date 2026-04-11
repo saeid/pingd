f7060dd8a547d75f995a9632e7aadcb977001b1b
Saeid Basirnia <git@basirnia.me>
2026-04-11 09:25:44 +0200

    Update AppDependencyManager

--- Sources/pingd/Core/AppDependencyManager.swift
+++ Sources/pingd/Core/AppDependencyManager.swift
@@ -2,22 +2,37 @@ import Vapor
 
 struct AppDependencies {
     let appInfo: AppInfo
-    let now: () -> Date
+    let now: @Sendable () -> Date
     let healthClient: HealthClient
     let authClient: AuthClient
     let tokenClient: TokenClient
     let userClient: UserClient
+    let authFeature: AuthFeature
+    let userFeature: UserFeature
+    let tokenFeature: TokenFeature
 }
 
 extension AppDependencies {
     static func live(with app: Application) -> AppDependencies {
-        AppDependencies(
+        let now: @Sendable () -> Date = { Date() }
+        let authClient = AuthClient.live()
+        let tokenClient = TokenClient.live(app: app)
+        let userClient = UserClient.live(app: app)
+        return AppDependencies(
             appInfo: AppInfo.current,
-            now: { Date() },
+            now: now,
             healthClient: HealthClient.live(app: app),
-            authClient: AuthClient.live(),
-            tokenClient: TokenClient.live(app: app),
-            userClient: UserClient.live(app: app)
+            authClient: authClient,
+            tokenClient: tokenClient,
+            userClient: userClient,
+            authFeature: AuthFeature.live(
+                userClient: userClient,
+                authClient: authClient,
+                tokenClient: tokenClient,
+                now: now
+            ),
+            userFeature: UserFeature.live(userClient: userClient),
+            tokenFeature: TokenFeature.live(tokenClient: tokenClient, userClient: userClient)
         )
     }
 
@@ -29,23 +44,35 @@ extension AppDependencies {
         tokenClient: TokenClient = .mock(),
         userClient: UserClient = .mock()
     ) -> AppDependencies {
-        AppDependencies(
+        let now: @Sendable () -> Date = { fixedNow }
+        return AppDependencies(
             appInfo: appInfo,
-            now: { fixedNow },
+            now: now,
             healthClient: healthClient,
             authClient: authClient,
             tokenClient: tokenClient,
-            userClient: userClient
+            userClient: userClient,
+            authFeature: AuthFeature.live(
+                userClient: userClient,
+                authClient: authClient,
+                tokenClient: tokenClient,
+                now: now
+            ),
+            userFeature: UserFeature.live(userClient: userClient),
+            tokenFeature: TokenFeature.live(tokenClient: tokenClient, userClient: userClient)
         )
     }
 
     func withReplacing(
         appInfo: AppInfo? = nil,
-        now: (() -> Date)? = nil,
+        now: (@Sendable () -> Date)? = nil,
         healthClient: HealthClient? = nil,
         authClient: AuthClient? = nil,
         tokenClient: TokenClient? = nil,
-        userClient: UserClient? = nil
+        userClient: UserClient? = nil,
+        authFeature: AuthFeature? = nil,
+        userFeature: UserFeature? = nil,
+        tokenFeature: TokenFeature? = nil
     ) -> AppDependencies {
         AppDependencies(
             appInfo: appInfo ?? self.appInfo,
@@ -53,7 +80,10 @@ extension AppDependencies {
             healthClient: healthClient ?? self.healthClient,
             authClient: authClient ?? self.authClient,
             tokenClient: tokenClient ?? self.tokenClient,
-            userClient: userClient ?? self.userClient
+            userClient: userClient ?? self.userClient,
+            authFeature: authFeature ?? self.authFeature,
+            userFeature: userFeature ?? self.userFeature,
+            tokenFeature: tokenFeature ?? self.tokenFeature
         )
     }
 }
