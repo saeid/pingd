8251cb697ce12ee1fe7fdfcfa7c3d92fb5adbb41
Saeid Basirnia <git@basirnia.me>
2026-04-17 13:11:01 +0200

    Add file serving middleware on server launch

--- Sources/pingd/Startup/configure.swift
+++ Sources/pingd/Startup/configure.swift
@@ -5,6 +5,7 @@ import Vapor
 
 // configures your application
 public func configure(_ app: Application) async throws {
+    app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory, defaultFile: "index.html"))
     app.middleware.use(RequestLoggerMiddleware())
 
     if app.environment == .testing {
