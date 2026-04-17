9d92d903a989a8c6f40c76e48c594ddca0ad714a
Saeid Basirnia <git@basirnia.me>
2026-04-17 23:35:57 +0200

    Read CLI config from data directory and set default server port to 7685

--- Sources/pingd-cli/Config.swift
+++ Sources/pingd-cli/Config.swift
@@ -6,25 +6,28 @@ struct CLIConfig: Codable {
 }
 
 enum ConfigManager {
-    private static var configDir: URL {
-        FileManager.default.homeDirectoryForCurrentUser.appendingPathComponent(".pingd")
+    private static let defaultServerURL = "http://localhost:7685"
+
+    private static var dataDir: URL {
+        let path = ProcessInfo.processInfo.environment["PINGD_DATA_DIR"] ?? "data"
+        return URL(fileURLWithPath: path, isDirectory: true)
     }
 
     private static var configFile: URL {
-        configDir.appendingPathComponent("config.json")
+        dataDir.appendingPathComponent("cli-config.json")
     }
 
     static func load() -> CLIConfig {
         guard let data = try? Data(contentsOf: configFile),
               let config = try? JSONDecoder().decode(CLIConfig.self, from: data)
         else {
-            return CLIConfig(serverURL: "http://localhost:8080", token: nil)
+            return CLIConfig(serverURL: defaultServerURL, token: nil)
         }
         return config
     }
 
     static func save(_ config: CLIConfig) throws {
-        try FileManager.default.createDirectory(at: configDir, withIntermediateDirectories: true)
+        try FileManager.default.createDirectory(at: dataDir, withIntermediateDirectories: true)
         let encoder = JSONEncoder()
         encoder.outputFormatting = .prettyPrinted
         let data = try encoder.encode(config)
